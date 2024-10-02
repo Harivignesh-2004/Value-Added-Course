300a7815da448e079d9cdf9c93b319d06cc1c19f
Haripriya Shanmugam <haripriyashanmugam92@gmail.com>
2024-10-02 22:19:03 +0530

    API routes- post and get  completed

--- models/router/crudrouter/index.js
+++ models/router/crudrouter/index.js
@@ -3,3 +3,31 @@ import Router from "express";
 const router = Router();
 
 export default router;
+
+
+//Student Model
+router.post("/student", async (request, response) => {
+    try {
+      const student = Student(request.body);
+      await student.save();
+      response.send("Student created successfully");
+    } catch (error) {
+      response.send("error");
+    }
+  });
+  router.get("/stud/:id", async (request, response) => {
+    try {
+      const students = await Student.findById(request.params.id);
+      response.send(students);
+    } catch (error) {
+      response.send(error);
+    }
+  });
+  router.get("/stud", async (request, response) => {
+    try {
+      const student = await Student.find();
+      response.send(student);
+    } catch (error) {
+      response.send("ERROR");
+    }
+  });
