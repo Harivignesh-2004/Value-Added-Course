da66918d3d8797beb7af38a72fe0c98933e6c03d
mogithaaduraisamy <mogithaaduraisamy2403@gmail.com>
2024-10-02 22:49:55 +0530

    added assignment post and get routes

--- models/router/crudrouter/index.js
+++ models/router/crudrouter/index.js
@@ -1,5 +1,6 @@
 import Router from "express";
 import Student from "../../Student.js";
+import Assignment from "../../Assignment.js";
 const router = Router();
 
 export default router;
@@ -26,6 +27,35 @@ router.get("/stud", async (request, response) => {
   try {
     const student = await Student.find();
     response.send(student);
+  } catch (error) {
+    response.send("ERROR");
+  }
+});
+
+//assignment model
+
+router.post("/assign", async (request, response) => {
+  try {
+    const assignment = Assignment(request.body);
+    await assignment.save();
+    response.send("Assignment work successfull");
+  } catch (error) {
+    response.send("error");
+  }
+});
+router.get("/assignget/:id", async (request, response) => {
+  try {
+    const assignment = await Assignment.findById(request.params.id);
+    response.send(assignment);
+  } catch (error) {
+    response.send(error);
+  }
+});
+
+router.get("/assignget", async (request, response) => {
+  try {
+    const assignemnt = await Assignment.find();
+    response.send(assignemnt);
   } catch (error) {
     response.send("ERROR");
   }
